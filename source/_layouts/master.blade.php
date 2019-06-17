cc53952f2e77f724e39e237a27275b78531e5d25
DELTA-CREATOR <51921541+DELTA-CREATOR@users.noreply.github.com>
2019-06-17 13:00:38 -0400

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -35,12 +35,7 @@
         </section>
     </article>
 
-    <footer>
-        <small>
-            This project is maintained by <a href="https://raniesantos.netlify.com">Ranie Santos</a>.
-            View the <a href="https://github.com/raniesantos/artisan-static">GitHub repo</a>.
-        </small>
-    </footer>
+ 
 
     <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
     @includeWhen($page->production, '_partials.analytics')
