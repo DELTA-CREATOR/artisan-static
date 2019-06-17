fd048431d73a5c1c208d091a4746f2deafeeff75
DELTA-CREATOR <51921541+DELTA-CREATOR@users.noreply.github.com>
2019-06-17 12:33:43 -0400

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -7,4 +7,4 @@
 
     <p>Our Time Has Come. We must reveal to the world the horror and injustice caused by alpha</p>
 
-<p1>our souls are bound to Omega, the goddess of Truth. but alpha has no soul. the only have hunger, hunger for the knoledge that they themselves are to cowerdice to see.</p1>
+    <p1>our souls are bound to Omega, the goddess of Truth. but alpha has no soul. the only have hunger, hunger for the knoledge that they themselves are to cowerdice to see.</p1>
