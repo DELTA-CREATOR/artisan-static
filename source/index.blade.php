3b97b8eecce7345079a0a2ae37ce4dc722c94ba6
Delta <Buttermeyo@gmail.com>
2019-06-17 14:09:12 -0400

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,6 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static for Jigsaw</h1>
+    <h1>Welcome to Delta-Net</h1>
 
-    <h2>Asset Compilation Test</h2>
-
-    <p>CSS test: <span class="test-css">text with border and padding</span></p>
-
-    <p>JavaScript test: <a href="#" class="test-js">click me</a></p>
-@endsection
+    <h1>please, i need to find the code. its important.<h1>
